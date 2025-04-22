ed545fea4cf0cfbe9cecdeafe188e68be2141fd3
Seudonym <wk170179@gmail.com>
2025-04-22 21:43:47 +0530

    feat: custom nvim config

---
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -0,0 +1,17 @@
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- FzfLua keymaps
+vim.keymap.set("n", "<leader>ff", "<CMD>FzfLua files<CR>", { desc = "Fuzzy find files in current directory" })
+vim.keymap.set("n", "<leader>fg", "<CMD>FzfLua live_grep<CR>", { desc = "Fuzzy find files in current directory" })
+vim.keymap.set("n", "<leader>fc", "<CMD>FzfLua files cwd=~/.config/nvim<CR>", { desc = "Fuzzy find files in current directory" })
+vim.keymap.set("n", "<leader>fw", "<CMD>FzfLua grep_cword<CR>", { desc = "Find current word" })
+vim.keymap.set("n", "<leader>fW", "<CMD>FzfLua grep_cWORD<CR>", { desc = "Find current WORD" })
+vim.keymap.set("n", "<leader><leader>", "<CMD>FzfLua buffers<CR>", { desc = "Find current WORD" })
+vim.keymap.set("n", "<leader>fb", "<CMD>FzfLua grep_curbuf<CR>", { desc = "Find current WORD" })
+vim.keymap.set("n", "<leader>fh", "<CMD>FzfLua helptags<CR>", { desc = "Find help" })
+
+
+-- LSP and formatting keymaps
+vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, { desc = "Open diagnostics window" })
+vim.keymap.set("n", "<leader>fm", function() require('conform').format() end, { desc = "Format the current buffer"   })
+
---
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -0,0 +1,8 @@
+return { 
+  'echasnovski/mini.statusline', 
+  version = '*',
+  dependencies = {
+    'echasnovski/mini.icons', 
+  },
+  opts = {}
+}
